a1918657d8a691220b06f312baf88c86b7cb6176
marco94gug <marco.guglielmino94@gmail.com>
2023-06-08 21:52:44 +0200

    feat: changed response with popular drinks

--- server/api/cocktail.ts
+++ server/api/cocktail.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const data = await $fetch("https://gintonic-api.vercel.app/drinks");
+  const data = await $fetch("https://gintonic-api.vercel.app/popular");
 
   if (event.node.req.method === "GET") {
     return data;
