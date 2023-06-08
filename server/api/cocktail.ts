26ee5858ea5c38faf33e2cb9f65bcfe191c8c413
marco94gug <marco.guglielmino94@gmail.com>
2023-06-08 21:48:16 +0200

    feat: use $fetch instead fetch

--- server/api/cocktail.ts
+++ server/api/cocktail.ts
@@ -1,8 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const data = await fetch("https://gintonic-api.vercel.app/drinks");
+  const data = await $fetch("https://gintonic-api.vercel.app/drinks");
 
   if (event.node.req.method === "GET") {
-    const res = await data.json();
-    return res;
+    return data;
   }
 });
