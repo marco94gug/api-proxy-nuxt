815dd42684de57c3884045fdf8127627e5efb9bb
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-06-09 11:00:23 +0200

    feat: enable cors on server dir

--- server/api/cocktail.ts
+++ server/api/cocktail.ts
@@ -1,17 +1,4 @@
 export default defineEventHandler(async (event) => {
-  setResponseHeaders(event, {
-    "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Expose-Headers": "*",
-  });
-  if (getMethod(event) === "OPTIONS") {
-    event.node.res.statusCode = 204;
-    event.node.res.statusMessage = "No Content.";
-    return "OK";
-  }
-
   const data = await $fetch("https://gintonic-api.vercel.app/popular");
 
   if (event.node.req.method === "GET") {
